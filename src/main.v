13025f3f4e556eff866f0644956e3969b7f1b3d5
kintrix007 <boldizsar02@gmail.com>
2023-03-16 16:30:12 +0100

    Added placeholder listeners to template

--- src/main.v
+++ src/main.v
@@ -14,6 +14,8 @@ fn main() {
 	mut client := vd.new(token: bot_token, intents: intents) or { panic('Could not authenticate') }
 
 	client.on_ready(on_ready)
+	client.on_message_create(on_message)
+	client.on_message_reaction_add(on_reaction)
 
 	println('Bot online...')
 	client.run().wait()
@@ -21,4 +23,16 @@ fn main() {
 
 fn on_ready(mut client vd.Client, ready &vd.Ready) {
 	println("Bot ready as '${ready.user.username}#${ready.user.discriminator}'.")
+}
+
+fn on_message(mut client vd.Client, message &vd.MessageCreate) {
+	if message.author.bot {
+		return
+	}
+}
+
+fn on_reaction(mut client vd.Client, reaction &vd.MessageReactionAdd) {
+	if reaction.member.user.bot {
+		return
+	}
 }
