9104a60f594861fa32696da55d9f60e4cfb74d83
kintrix007 <boldizsar02@gmail.com>
2023-03-16 16:26:07 +0100

    Made basic bot template

--- src/main.v
+++ src/main.v
@@ -1,5 +1,24 @@
 module main
 
+import os
+import discordv as vd
+import discordv.gateway
+
+const bot_token = os.getenv('BOT_TOKEN')
+
 fn main() {
-	println('Hello World!')
+	if bot_token == '' {
+		panic("Please make sure you have the 'BOT_TOKEN' env variable set.")
+	}
+	intents := gateway.all_allowed
+	mut client := vd.new(token: bot_token, intents: intents) or { panic('Could not authenticate') }
+
+	client.on_ready(on_ready)
+
+	println('Bot online...')
+	client.run().wait()
+}
+
+fn on_ready(mut client vd.Client, ready &vd.Ready) {
+	println("Bot ready as '${ready.user.username}#${ready.user.discriminator}'.")
 }
